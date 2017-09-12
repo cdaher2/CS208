0fbd3369240caa9bd77c4c70532a9f2ce1612274
cdaher2 <c_daher2@post.massbay.edu>
2017-09-12 17:13:27 -0400

    mcat.c update

--- projectsCD/cprimer/mcat.c
+++ projectsCD/cprimer/mcat.c
@@ -2,7 +2,15 @@
 //an alternate version of cat
 //Christian Daher 9/11/17
 
-int main(int argc, char* argv[]) {
-	open(argv, 0);
-//	write(1, );
+char buffer = 'F';
+
+int main(int argc, char** argv) {
+	int file = open(argv[1]);
+	int i;
+	write(1, &buffer, 1);
+	while (read(file, &buffer, 1) != -1) {
+		write(1, &buffer, 1);
+	}
+	write(1, &buffer, 1);
+	return 0;
 }

14966c8f1eed406f168bcf47e6f2c23383021320
cdaher2 <c_daher2@post.massbay.edu>
2017-09-12 17:32:01 -0400

    fixed mcat.c

--- projectsCD/cprimer/mcat.c
+++ projectsCD/cprimer/mcat.c
@@ -1,16 +1,14 @@
 //mcat.c
 //an alternate version of cat
 //Christian Daher 9/11/17
+#include <fcntl.h>
 
-char buffer = 'F';
+char buffer;
 
 int main(int argc, char** argv) {
-	int file = open(argv[1]);
-	int i;
-	write(1, &buffer, 1);
-	while (read(file, &buffer, 1) != -1) {
+	int file = open(argv[1], O_RDONLY);
+	while (read(file, &buffer, 1)) {
 		write(1, &buffer, 1);
 	}
-	write(1, &buffer, 1);
 	return 0;
 }

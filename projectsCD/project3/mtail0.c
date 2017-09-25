ce1c749050885c1c39142ca85f98adbfc8ad9aa1
cdaher2 <c_daher2@post.massbay.edu>
2017-09-25 15:38:29 -0400

    yeah

--- projectsCD/project3/mtail0.c
+++ projectsCD/project3/mtail0.c
@@ -3,17 +3,9 @@
 //Christian Daher 9/24/2017
 #define O_RDONLY 0000
 
+off_t offset = lseek(int f, int_t offset, int from_where);
 char buffer;
 
 int main(int argc, char** argv) {
 	int file = open(argv[1], O_RDONLY);
-	int i = 0;
-	while (read (file, &buffer, 1)) {
-		if (buffer == \n) {
-			++i;
-		}
-	}
-	int i2 = i - 10;
 	
-	while (i2 < i) {
-		read(file, &buffer, 1

18bcb6d71bfb88474bf889895862e142ce96805f
cdaher2 <c_daher2@post.massbay.edu>
2017-10-02 11:02:08 -0400

    added more files

---
+++ projectsCD/project5/mls0.c
@@ -0,0 +1,12 @@
+//a version of ls written in c
+//Christian Daher 10/2/17
+#include <dirent.h>
+
+DIR *opendir(const char *name);
+struct dirent *readdir(DIR *dirp);
+
+typedef struct dirent {
+	
+} buffer;
+
+

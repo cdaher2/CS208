99425754c6e6c64a9129fafbd41ea0d048878fc7
cdaher2 <c_daher2@post.massbay.edu>
2017-10-02 11:01:13 -0400

    project5/4

--- projectsCD/project1b/mcat1b.c
+++ projectsCD/project1b/mcat1b.c
@@ -21,24 +21,25 @@ int main(int argc, char** argv) {
 				ops = 0;
 				break;
 		}
-	}
-	file = open(argv[optind], 0000);
-	int i = 1;
-	if (ops == 1) {
-		write(1, itoa(i, 5), 5);
-		write(1, " ", 1);
-	}
-	while (read(file, &buffer, 1)){
-		write (1, &buffer, 1);
-		if (buffer == '\n') {
-			i = i + 1;
-			if (ops == 1) {
-				write(1, itoa(i, 5), 5);
-				write(1, " ", 1);
+	
+		file = open(argv[optind], 0000);
+		int i = 1;
+		if (ops == 1) {
+			write(1, itoa(i, 5), 5);
+			write(1, " ", 1);
+		}
+		while (read(file, &buffer, 1)){
+			write (1, &buffer, 1);
+			if (buffer == '\n') {
+				i = i + 1;
+				if (ops == 1) {
+					write(1, itoa(i, 5), 5);
+					write(1, " ", 1);
+				}
 			}
 		}
+		write (1, "\n", 1);
+		close (file);
+		return 0;
 	}
-	write (1, "\n", 1);
-	close (file);
-	return 0;
 }

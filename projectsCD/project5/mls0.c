79cad0938b972d35cb59ce1831c18b2664b354c4
cdaher2 <c_daher2@post.massbay.edu>
2017-10-03 17:26:17 -0400

    mls0

--- projectsCD/project5/mls0.c
+++ projectsCD/project5/mls0.c
@@ -1,12 +1,19 @@
 //a version of ls written in c
 //Christian Daher 10/2/17
 #include <dirent.h>
+#define NULL (void *)0
 
-DIR *opendir(const char *name);
-struct dirent *readdir(DIR *dirp);
+size_t strlen(const char *s);
 
-typedef struct dirent {
-	
-} buffer;
+int main(int argc, char** argv) {
+	struct dirent* buffer;
+	DIR* cd;
 
+	cd = opendir(".");
 
+	while(buffer = readdir(cd)) {
+		write (1, buffer->d_name, strlen(buffer->d_name));
+		write (1, "\n", 1);
+	}
+	return 0;
+}
